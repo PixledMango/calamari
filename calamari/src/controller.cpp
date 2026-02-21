ec78e34d43436fe4a3f28ac2af91bff811c1a171
PixledMango <gage.hepworth@gmail.com>
2026-02-21 11:58:03 -0700

    Initial Commit

---
+++ calamari/src/controller.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+int LED = 2;
+
+void setup()
+{
+  pinMode(LED, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(LED, HIGH);
+  delay(5000);
+  digitalWrite(LED, LOW);
+  delay(5000);
+}
